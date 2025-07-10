f36614d6334aa78cfcec7e856ada91f9f5aefa61
Yusriyah Firjatullah <57206360+yusriyahf@users.noreply.github.com>
2025-07-10 21:38:46 +0700

    integrate admin template

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard/index');
+});
+
+Route::get('/tipologi', function () {
+    return view('tipologi/index');
 });
